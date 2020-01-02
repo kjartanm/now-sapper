8b9257a4a13c169920b9ab545d9c5869c7ea50c4
Kjartan Rekdal Müller <kjartan@muller.no>
2020-01-02 12:21:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ exports.build = async ({
   const distDir = (config.distDir)?config.distDir + '/': '';
   const spawnOpts = getSpawnOptions(meta, nodeVersion)
   const prodDependencies = await npmBuild(config, entrypointDir, spawnOpts, meta)
+  console.log("prodDependencies", prodDependencies)
   const launcherFiles = getLauncherFiles(entrypointDir)
   const staticFiles = await globAndPrefix(entrypointDir, 'static')
   const applicationFiles = await globAndPrefix(entrypointDir, '__sapper__')
